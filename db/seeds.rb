b51f8b3f75dd4a64994cecff8ebb2022731feaf1
Victoria Hale <vic.hale74@gmail.com>
2019-11-10 18:29:48 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,3 @@ Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
 
-namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
-    Student.create_table
-  end
-end
